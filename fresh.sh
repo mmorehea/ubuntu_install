fde9334c3ab22a6564eff02280f984544365b667
Michael Morehead <mmorehea@mix.wvu.edu>
2016-06-01 14:20:05 -0400

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -15,6 +15,7 @@ sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
 sudo apt-get install browser-plugin-freshplayer-pepperflash
 
+sudo apt-get install matlab-support
 sudo apt-get install meshlab
 
 
